3ee4bcad4ec1d6ac868d42da7fc1611e427c0958
gnshmule <gnshmule@gmail.com>
2024-06-06 14:49:27 +0530

    commit new chnages

---
+++ src/main/java/com/example/devopsdemo1/Devopsdemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.devopsdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devopsdemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Devopsdemo1Application.class, args);
+	}
+
+}
